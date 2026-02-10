1474caf887f2cb08de389234360462d5b1921468
Artem <la.artem.ser@gmail.com>
2026-02-10 20:50:50 +0200

    feat: add daily stargazers statistics and chart to IvyInsightsApp for the last 365 days

--- Services/IDatabaseService.cs
+++ Services/IDatabaseService.cs
@@ -6,5 +6,6 @@ public interface IDatabaseService
 {
     Task<List<DailyDownloadStats>> GetDailyDownloadStatsAsync(int days = 30, CancellationToken cancellationToken = default);
     Task<List<GithubStarsStats>> GetGithubStarsStatsAsync(int days = 30, CancellationToken cancellationToken = default);
+    Task<List<GithubStargazersDailyStats>> GetGithubStargazersDailyStatsAsync(int days = 30, CancellationToken cancellationToken = default);
 }
 
